bc1c2cf19a495c859660eff4ff351a3d8b0e65b2
Kevin Deakers <kmdeakers@mac.com>
2023-04-12 14:50:43 -0700

    write first test for sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,21 @@
 const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Takes dataToUpdate which is an object that contains only
+ * the data that we want to update as first argument. 
+ * {firstName: 'Aliya', age: 32}
+ * 
+ * As a second argument, it takes jsToSql, which is an object with keys that have
+ * a value of the key name converted from camel case to snake case.
+ * { firstName: 'first_name' }
+ * 
+ * Returns an object with a key of setCols which equals the name
+ * of the columns that are being changed, with each column equal
+ * to the corresponding values in the values key.
+ * 
+ * { setCols: '"first_name"=$1, "age"=$2', values: [ 'Aliya', 32 ] }
+ * 
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,4 +1,13 @@
 "use strict"
 
 const { sqlForPartialUpdate } = require("./sql");
-const { BadRequestError } = require("../expressError")
+const { BadRequestError } = require("../expressError");
+
+describe("Generate sql helper object for partial update", function() {
+    test("works: valid data supplied", function() {
+        const result = sqlForPartialUpdate({firstName: 'Aliya', age: 32},
+        {firstName: 'first_name'});
+        expect(result).toEqual({ setCols: '"first_name"=$1, "age"=$2', 
+                                values: [ 'Aliya', 32 ]});
+    });
+});
